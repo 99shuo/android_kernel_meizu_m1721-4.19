ad7f09660a9c122d402568092e2cfa06eff619e1
99shuo <51776223+99shuo@users.noreply.github.com>
2023-12-05 05:03:11 +0000

    Revert "techpack: ASoC: adapt to new component"
    This reverts commit f43e261c3330a5890d231be3994eee7dd44eaa13.

--- techpack/audio/include/dsp/apr_audio-v2.h
+++ techpack/audio/include/dsp/apr_audio-v2.h
@@ -2026,30 +2026,6 @@ struct afe_rtac_user_data_get_v3 {
 } __packed;
 
 #define AFE_PORT_CMD_SET_PARAM_V2	0x000100EF
-struct afe_port_param_data_v2 {
-        u32 module_id;
-/* ID of the module to be configured.
- * Supported values: Valid module ID
- */
-
-u32 param_id;
-/* ID of the parameter corresponding to the supported parameters
- * for the module ID.
- * Supported values: Valid parameter ID
- */
-
-u16 param_size;
-/* Actual size of the data for the
- * module_id/param_id pair. The size is a
- * multiple of four bytes.
- * Supported values: > 0
- */
-
-u16 reserved;
-/* This field must be set to zero.
- */
-} __packed;
-
 struct afe_port_cmd_set_param_v2 {
 	/* APR Header */
 	struct apr_hdr apr_hdr;
